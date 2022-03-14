4ce11ec6d6a3823ac3ae9d78f3b552744470f467
Yi Yao <leaf_yao8621@hotmail.com>
2022-03-14 14:54:57 -0700

    FWP C

--- script/test.py
+++ script/test.py
@@ -1,4 +1,8 @@
 import dijkstra.cpp
+import dijkstra.c
 
 if __name__ == "__main__":
+    print("CPP")
     print(dijkstra.cpp.distance("../graph/test.graph", 0, 2, True))
+    print("C")
+    print(dijkstra.c.distance("../graph/test.graph", 0, 2, True))
