ab52531ef17be9a2bcba72b7a5d7de47400089e4
Yi Yao <leaf_yao8621@hotmail.com>
2022-03-15 14:15:42 -0700

    Added bench script

--- script/bench.py
+++ script/bench.py
@@ -1,7 +1,19 @@
 import timeit
 import dijkstra.cpp
+import dijkstra.c
+import dijkstra.native
 
 if __name__ == "__main__":
-    cpp = lambda: dijkstra.cpp.distance("../graph/small.graph", 0, 20)
-    print("CPP")
-    print(timeit.timeit(cpp))
+    suite = ["small", "medium", "large"]
+    iter = 100
+    for i in suite:
+        print(f"Graph {i}:")
+        cpp = lambda: dijkstra.cpp.distance(f"../graph/{i}.graph", 0, 20)
+        print(f"CPP Runtime ({iter} iterations):")
+        print(f"{timeit.timeit(cpp, number=iter)} seconds")
+        c = lambda: dijkstra.c.distance(f"../graph/{i}.graph", 0, 20)
+        print(f"C Runtime ({iter} iterations):")
+        print(f"{timeit.timeit(c, number=iter)} seconds")
+        native = lambda: dijkstra.native.distance(f"../graph/{i}.graph", 0, 20)
+        print(f"NATIVE Runtime ({iter} iterations):")
+        print(f"{timeit.timeit(native, number=iter)} seconds")
