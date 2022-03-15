71eae97702d29073dcb85ceb9e72e6b1b72533ed
Yi Yao <leaf_yao8621@hotmail.com>
2022-03-15 14:05:10 -0700

    Modified scripts

--- script/test.py
+++ script/test.py
@@ -4,8 +4,8 @@ import dijkstra.native
 
 if __name__ == "__main__":
     print("CPP")
-    print(dijkstra.cpp.distance("../graph/test.graph", 0, 2, True))
+    print(dijkstra.cpp.distance("../graph/test.graph", 0, 2, verbose=True))
     print("C")
-    print(dijkstra.c.distance("../graph/test.graph", 0, 2, True))
+    print(dijkstra.c.distance("../graph/test.graph", 0, 2, verbose=True))
     print("NATIVE")
-    print(dijkstra.native.distance("../graph/test.graph", 0, 2, True))
+    print(dijkstra.native.distance("../graph/test.graph", 0, 2, verbose=True))
